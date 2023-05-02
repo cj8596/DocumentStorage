514a34de2ddf594a5dd1c8953cf0b8cd2b0969c2
Cristin <cristinjohnson96@gmail.com>
2023-05-02 19:15:35 +0530

    Updating application properties and main file

---
+++ src/test/java/com/cristin/project/DocumentStorage/DocumentStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cristin.project.DocumentStorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
